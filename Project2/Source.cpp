89c632f0373a2cd6c71fe7ecec59133c9d3b3dee
Dennis K <golden_talon@hotmail.de>
2019-02-12 16:38:54 +0100

    Add project files.

---
+++ Project2/Source.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+#include <limits>
+
+int main()
+{
+	std::cout << "Gebe 1 oder 2 ein: ";
+	int eingabe1{ 0 };
+	std::cin >> eingabe1;
+
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+	std::cout << "Und jetzt deinen vollen Namen: ";
+	std::string name;
+	std::getline(std::cin, name);
+	std::cout << "Dein Wahl war " << eingabe1 << " und dein voller Name lautet " << name << ". \n";
+	return 0;
+}
